0be387b31d5195dbbf7062efa548dfcb6bc67291
Slavi Dimitrov <slavidimitrov54@gmail.com>
2024-08-28 11:05:04 +0300

    Init a sql database and map the data into a meals component

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,16 +1,28 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const meals = await getMeals();
+
   return (
-    <main>
-      <h1 style={{ textAlign: "center", color: "white" }}>
-        This is our Main Meals Page
-      </h1>
-      <p style={{ textAlign: "center" }}>
-        <Link style={{ color: "white" }} href="/meals/share">
-          Share food
-        </Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, creates
+          <span className={classes.highlight}> by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href={"/meals/share"}>Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
   );
 }
