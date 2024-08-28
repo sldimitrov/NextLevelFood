166e923a002ec61f839bb79864247762beb4872c
Slavi Dimitrov <slavidimitrov54@gmail.com>
2024-08-28 15:15:46 +0300

    Working with Server Action Responses & useFormState
    add an error message at the end of the form

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,8 +1,15 @@
+"use client";
+
+import { useFormState } from "react-dom";
+
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
+import MealsFormSubmit from "../meals-form-submit";
 import { shareMeal } from "@/lib/action";
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
+
   return (
     <>
       <header className={classes.header}>
@@ -12,7 +19,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -41,8 +48,9 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
